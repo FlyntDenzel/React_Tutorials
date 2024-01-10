a9999e662a0a54516b129d1722a120a06f05d549
Nchang Denzel <93846700+FlyntDenzel@users.noreply.github.com>
2024-01-10 23:37:01 +0100

    created entry component

--- src/Emoji App/Emoji.jsx
+++ src/Emoji App/Emoji.jsx
@@ -5,7 +5,7 @@ const Emoji = () => {
   return (
     <div>
         <div className='flex justify-center items-center pt-2'>
-            <h1 className='bg-[#00ffc8] w-2/3 p-2 text-5xl font-bold flex justify-center items-center text-white'>EmojiPedia</h1>
+            <h1 className='bg-[#00ffc8] w-2/3 p-2 text-5xl font-bold flex justify-center items-center text-white mb-3'>EmojiPedia</h1>
         </div>
         <Entry />
     </div>
--- src/Emoji App/Entry.jsx
+++ src/Emoji App/Entry.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Entry = () => {
   return (
-    <div>Entry</div>
+    <div className='border bg-white w-2/6 flex justify-center items-center m-3 rounded-lg' contentEditable="true">
+        <div className=''>
+            <h1 >emoji</h1>
+            <h2 >Name</h2>
+            <h2 >description</h2>
+        </div>
+    </div>
   )
 }
 
