f7bf67b5efd8c1b7a71d717bdf458b1dd9e76370
Nchang Denzel <93846700+FlyntDenzel@users.noreply.github.com>
2024-01-10 20:14:33 +0100

    imported a js file using the default method

--- src/components/Components.jsx
+++ src/components/Components.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Add from '../data/calculator'
+import Substract from '../data/calculator'
+import Multiply from '../data/calculator'
+import Divide from '../data/calculator'
 
 const Components = () => {
   return (
-    <div>Components</div>
+    <div>
+      <ul className='p-3'>
+        <li>{Add(1, 3)}</li>
+        <li>{Substract(1, 3)}</li>
+        <li>{Multiply(1, 3)}</li>
+        <li>{Divide(1, 3)}</li>
+      </ul>
+    </div>
   )
 }
 
---
+++ src/data/calculator.js
@@ -0,0 +1,18 @@
+function Add(n1, n2)  {
+    return n1 + n2;
+}
+
+function Substract(n1, n2)  {
+    return n1 - n2;
+}
+
+function Multiply(n1, n2)  {
+    return n1 * n2;
+}
+
+function Divide(n1, n2)  {
+    return n1 / n2;
+}
+
+export default Add;
+export {Substract, Multiply, Divide};
