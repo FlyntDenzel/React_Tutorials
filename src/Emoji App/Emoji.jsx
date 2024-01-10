dcb9044bfc0b436f2bed748c4eecb8fa86bb2b30
Nchang Denzel <93846700+FlyntDenzel@users.noreply.github.com>
2024-01-10 23:53:36 +0100

    mapped data from the emojipedia array to the entry component

--- src/Emoji App/Emoji.jsx
+++ src/Emoji App/Emoji.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import Entry from './Entry'
+import emojipedia from './emojipedia'
+
+function viewEmoji(emoji) {
+    return <Entry 
+        key = {emoji.id}
+        image = {emoji.emoji}
+        name = {emoji.name}
+        meaning = {emoji.meaning}
+    /> 
+}
 
 const Emoji = () => {
   return (
-    <div>
+    <div >
         <div className='flex justify-center items-center pt-2'>
             <h1 className='bg-[#00ffc8] w-2/3 p-2 text-5xl font-bold flex justify-center items-center text-white mb-3'>EmojiPedia</h1>
         </div>
-        <Entry />
+        <div className='grid grid-cols-3'>
+            {emojipedia.map(viewEmoji)}
+        </div>
     </div>
   )
 }
--- src/Emoji App/Entry.jsx
+++ src/Emoji App/Entry.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const Entry = () => {
+const Entry = (props) => {
   return (
-    <div className='border bg-white w-2/6 flex justify-center items-center m-3 rounded-lg' contentEditable="true">
+    <div className='border bg-white w-11/12 flex justify-center items-center m-3 rounded-lg p-3'>
         <div className=''>
-            <h1 >emoji</h1>
-            <h2 >Name</h2>
-            <h2 >description</h2>
+            <h1 className='text-5xl flex justify-center items-center py-2'>{props.image}</h1>
+            <h2 className='flex justify-center items-center font-bold text-2xl'>{props.name}</h2>
+            <h2 className='p-3'>{props.meaning}</h2>
         </div>
     </div>
   )
