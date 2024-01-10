bfe00bb3a34af82d4ed837c6705727207c9cf9de
Nchang Denzel <93846700+FlyntDenzel@users.noreply.github.com>
2024-01-10 20:50:30 +0100

    styled the note component

--- src/Keeper App/Note.jsx
+++ src/Keeper App/Note.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Note = () => {
   return (
-    <div className=' shadow-xl mb-3 w-1/4 m-3 p-3 rounded-lg' contentEditable="true">
+    <div className=' shadow-xl mb-3 w-1/4 m-3 p-3 rounded-lg border border-gray' contentEditable="true">
         <h1 className='font-bold'>This is the Note Title</h1>
         <p>This is the Note Content</p>
     </div>
