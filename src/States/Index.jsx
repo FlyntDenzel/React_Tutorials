bb51fbb90e17df882f5d1ae440a7138bf23898ab
Nchang Denzel <93846700+FlyntDenzel@users.noreply.github.com>
2024-01-11 18:49:34 +0100

    added the decrease function

--- src/States/Index.jsx
+++ src/States/Index.jsx
@@ -1,18 +1,20 @@
 import React, {useState} from 'react'
 
 const Index = () => {
-  const [count, setCount] = useState(0);
 
-  const increase = () =>{
-    setCount(count + 1);
+  const [count, setCount] = useState(0)
+  const increase = () => {
+    setCount(count + 1)
   }
 
+  const decrease = () => {
+    setCount(count - 1)
+  }
   return (
-    <div className='flex justify-center items-center'>
-      <div className=' justify-center items-center'>
-        <h2 className='text-6xl'>{count}</h2>
-        <button className='bg-blue-300 p-2 text-3xl px-2 rounded-lg' onClick={increase}>+</button>
-      </div>
+    <div>
+      <h1 className='text-4xl'>{count}</h1>
+      <button className='bg-blue-300 p-2 px-2 mx-2 rounded-lg' onClick={decrease}>-</button>
+      <button className='bg-blue-300 p-2 px-2 mx-2 rounded-lg' onClick={increase}>+</button>
     </div>
   )
 }
