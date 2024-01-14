bb9a95ffbf5ff51eb5b473c976119fca57d1172d
Nchang Denzel <93846700+FlyntDenzel@users.noreply.github.com>
2024-01-14 22:54:38 +0100

    learned how to destructure arrays and objects

--- src/Destructuring/Array.jsx
+++ src/Destructuring/Array.jsx
@@ -1,32 +1,53 @@
 import React from 'react'
+import animals from '../data/data';
 
-const Array = () => {
+// Destructuring Arrays
+// console.log(data);
+// const [cat, dog] = animals;
+// console.log(cat);
+
+// Destructuring Objects
+// const {name, sound, foodRequirements: {food, water}} = cat
+// console.log(water)
+// console.log(dog);
+
+// const Array = () => {
+//   return (
+//     <>
+//     <table>
+//       <tr>
+//         <th>Brand</th>
+//         <th>Top Speed</th>
+//         <th>Top Colour</th>
+//       </tr>
+//       <tr>
+//         <td>x</td>
+//         <td>y</td>
+//         <td>z</td>
+//       </tr>
+//       <tr>
+//         <td>x1</td>
+//         <td>y1</td>
+//         <td>z1</td>
+//       </tr>
+//       <tr>
+//         <td>x2</td>
+//         <td>y2</td>
+//         <td>z2</td>
+//       </tr>
+//     </table>
+//     </>
+//   )
+// }
+
+// export default Array
+
+const Destructure = () => {
   return (
     <>
-    <table>
-      <tr>
-        <th>Brand</th>
-        <th>Top Speed</th>
-        <th>Top Colour</th>
-      </tr>
-      <tr>
-        <td>x</td>
-        <td>y</td>
-        <td>z</td>
-      </tr>
-      <tr>
-        <td>x1</td>
-        <td>y1</td>
-        <td>z1</td>
-      </tr>
-      <tr>
-        <td>x2</td>
-        <td>y2</td>
-        <td>z2</td>
-      </tr>
-    </table>
+
     </>
   )
 }
 
-export default Array
+export default Destructure
