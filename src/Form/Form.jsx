afe5e29a6f4dd759070414230e0e8267ff6f5b2a
Nchang Denzel <93846700+FlyntDenzel@users.noreply.github.com>
2024-01-15 09:41:19 +0100

    learned about controlled input

--- src/Form/Form.jsx
+++ src/Form/Form.jsx
@@ -3,18 +3,25 @@ import React, { useState } from 'react'
 const Form = () => {
 
     const [name, setName] = useState("")
+    const [text, setText] = useState("")
 
     const handleChange = (event) =>{
-        console.log(event.target.value);
+        // console.log(event.target.value);
+        setName(event.target.value)
+    }
+
+    const handleClick = () => {
+        setText(name)
+        setName("")
     }
     return (
         <>
         <div className='flex items-center justify-center min-h-screen bg-blue-400'>
             <div className=''>
-                <h1 className='text-4xl'>Hello {name}</h1>
+                <h1 className='text-4xl'>Hello {text}</h1>
                 <div className='grid'>
-                    <input type="text" placeholder="What's your name" onChange={handleChange}/>
-                    <button className='p-2 bg-blue-300 rounded-lg mx-2 my-2'>Submit</button>
+                    <input type="text" placeholder="What's your name" value={name} onChange={handleChange}/>
+                    <button className='p-2 bg-blue-300 rounded-lg mx-2 my-2' onClick={handleClick}>Submit</button>
                 </div>
             </div>
         </div>
