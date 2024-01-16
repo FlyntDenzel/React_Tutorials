80edb5b0cbf17c59cddb85d73dfd5743cff80376
Nchang Denzel <93846700+FlyntDenzel@users.noreply.github.com>
2024-01-17 00:24:51 +0100

    created a basic todo list with mapping and spread operator

--- src/Todo-List/Todo.jsx
+++ src/Todo-List/Todo.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Todo = () => {
+    const [text, setText] = useState("");
+    const [item, setItem] = useState([]);
+
+    const handleChange = (event) => {
+        const todo = event.target.value;
+        // console.log(todo);
+        setText(todo)
+    }
+
+    const addTodo = (event) =>{
+        event.preventDefault();
+        setItem((prevItem) => {
+            return [...prevItem, text]
+        })
+        setText("")
+    }
+
   return (
     <>
         <div className='flex justify-center items-center min-h-screen bg-blue-300'>
@@ -9,12 +26,17 @@ const Todo = () => {
                     <h1 className='bg-gray-500 text-white font-bold p-2 rounded-lg mt-3'>TODO LIST</h1>
                 </div>
                 <div className='flex justify-center items-center mt-20'>
-                    <form>
-                        <input type="text" className='bg-gray-100 mx-4 p-2 border border-gray-400 rounded-md' />
-                        <button className='p-2 bg-blue-300 rounded-md '>ADD</button>
+                    <form >
+                        <input type="text" className='bg-gray-100 mx-4 p-2 border border-gray-400 rounded-md' value={text} onChange={handleChange} name='text'/>
+                        <button className='p-2 bg-blue-300 rounded-md ' onClick={addTodo}>ADD</button>
                         <ul className='m-3'>
-                            <li>hey</li>
-                            <li>hey</li>
+                            {
+                                item.map((newItem) => {
+                                    return (
+                                        <li>{newItem}</li>
+                                    )
+                                })
+                            }
                         </ul>
                     </form>
                 </div>
